308249d40b4bba61ec9a7d2db73bb37ef38ac41c
E Ek <eek.datasc@gmail.com>
2026-03-24 06:11:49 +0000

    Introduction Java Date and Time

---
+++ OOPJ/src/LocalDateTimeExample.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class LocalDateTimeExample {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("Current date and time: " + today);
+    }
+}
---
+++ OOPJ/src/ZoneDateTimeExample.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+public class ZoneDateTimeExample {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println("Current date and time with time zone: " + now);
+    }
+}
